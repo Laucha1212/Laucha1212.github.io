6b42253ca39f113cdb4c6cc0c8add2cd059be69d
Lautaro Perez <67768232+Laucha1212@users.noreply.github.com>
2023-07-27 01:16:22 -0300

    responsive +retoques

--- portafolio/portafolio.js
+++ portafolio/portafolio.js
@@ -9,6 +9,7 @@ function mostrarTexto() {
 }
 
 
+//FUNCION DE MODAL
 document.addEventListener("click",function (e){
     if(e.target.classList.contains("gallery-item")){
         const src = e.target.getAttribute("src");
